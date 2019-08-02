774c6776f4d17bb7289ec88546d5b3dd025f83e3
Alejandro Mercado <mercadoalex@gmail.com>
2019-08-02 13:28:44 -0500

    sitio angular con routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',component: PortafolioComponent},
+  { path:'about',component: AboutComponent},
+  { path:'item',component: ItemComponent},
+  { path:'**',pathMatch:'full', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
